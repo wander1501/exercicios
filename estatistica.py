8779f4364983048030fc7d96067f24f5b5b5c47d
wander1501 <47144641+wander1501@users.noreply.github.com>
2019-02-11 17:02:59 -0200

    Create estatistica.py

---
+++ estatistica.py
@@ -0,0 +1,55 @@
+# Autor: Wander dos Santos Vasconcellos
+#Data: 11/02/2019
+#
+import csv
+import sys
+import os
+
+def read_csv(filePath):
+
+    table = list()
+    with open(filePath) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            table.append(row)
+    return table
+
+def calcula_probabilidade_de_viver(idade, sexo, classe, passageiros):
+
+    totalPassageirosVivos = len([passgVivos for passgVivos in passageiros if passgVivos["Age"] == idade and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe and passgVivos["Survived"] == "1"])
+    totalPassageiros = len([passgVivos for passgVivos in passageiros if passgVivos["Age"] == idade and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe])
+    return totalPassageirosVivos / totalPassageiros
+
+def exibe_probabilidade(passg, passageiros):
+
+    probabilidadeDeVida = calcula_probabilidade_de_viver(passg["Age"], passg["Sex"], passg["Pclass"], passageiros)
+    print("A probabilidade de sobreviver do(a) passageiro(a) {} era de: {:3.2%}".format(passg["Name"], probabilidadeDeVida))
+
+titanic_tbl = read_csv("D:/Setor/Python_Fonts/titanic/train.csv")
+id = ''
+passageiro = []
+probabilidadeDeVida = 0.0
+while (id == ''):
+    id = input(
+        "Informe o id do passageiro, ou 'T' para calcular a probabilidade de sobrevivência de todos, ou 'S' para Sair: ").upper()
+    if id == 'S':
+        print("Programa encerrado!!")
+        sys.exit()
+    elif id == 'T':
+        for passenger in titanic_tbl:
+            exibe_probabilidade(passenger, titanic_tbl)
+        print("Total de passageiros: ", len(titanic_tbl))
+        sys.exit()
+    passageiro = [passg for passg in titanic_tbl if passg["PassengerId"] == id]
+    if len(passageiro) == 0:
+        id = ''
+        print("Id não encontrado!!")
+        os.system("pause")
+    elif len(passageiro) > 1:
+        id = ''
+        print("Existem várias ocorrências com este Id!!")
+        os.system("pause")
+    else:
+        id = ''
+        traveler = passageiro[0]
+        exibe_probabilidade(traveler, titanic_tbl)
