b3008b18fbedeaf8a0aee9868dea70204746ddeb
wander1501 <47144641+wander1501@users.noreply.github.com>
2019-02-13 19:04:26 -0200

    Update estatistica.py
    Introdução do intervalo de idade para calcular a probabilidade de viver de um passageiro e reorganização do código.

--- estatistica.py
+++ estatistica.py
@@ -1,5 +1,7 @@
 # Autor: Wander dos Santos Vasconcellos
 #Data: 11/02/2019
+#Alterações:
+#13/02/2019 - Inserção do intervalo de idade para o cálculo da probabilidade de viver de um passageiro.
 #
 import csv
 import sys
@@ -14,10 +16,23 @@ def read_csv(filePath):
             table.append(row)
     return table
 
+def is_float(idad):
+
+    try:
+        float(idad)
+        return True
+    except ValueError:
+        return False
+
 def calcula_probabilidade_de_viver(idade, sexo, classe, passageiros):
 
-    totalPassageirosVivos = len([passgVivos for passgVivos in passageiros if passgVivos["Age"] == idade and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe and passgVivos["Survived"] == "1"])
-    totalPassageiros = len([passgVivos for passgVivos in passageiros if passgVivos["Age"] == idade and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe])
+    if is_float(idade):
+        totalPassageirosVivos = len([passgVivos for passgVivos in passageiros if is_float(passgVivos["Age"]) and float(passgVivos["Age"]) >= (float(idade)-1) and float(passgVivos["Age"]) <= (float(idade)+1) and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe and passgVivos["Survived"] == "1"])
+        totalPassageiros = len([passgVivos for passgVivos in passageiros if is_float(passgVivos["Age"]) and float(passgVivos["Age"]) >= (float(idade)-1) and float(passgVivos["Age"]) <= (float(idade)+1) and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe])
+    else:
+        totalPassageirosVivos = len([passgVivos for passgVivos in passageiros if passgVivos["Age"] == idade and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe and passgVivos["Survived"] == "1"])
+        totalPassageiros = len([passgVivos for passgVivos in passageiros if passgVivos["Age"] == idade and passgVivos["Sex"] == sexo and passgVivos["Pclass"] == classe])
+
     return totalPassageirosVivos / totalPassageiros
 
 def exibe_probabilidade(passg, passageiros):
@@ -29,18 +44,23 @@ titanic_tbl = read_csv("D:/Setor/Python_Fonts/titanic/train.csv")
 id = ''
 passageiro = []
 probabilidadeDeVida = 0.0
+
 while (id == ''):
-    id = input(
-        "Informe o id do passageiro, ou 'T' para calcular a probabilidade de sobrevivência de todos, ou 'S' para Sair: ").upper()
+    
+    id = input("Informe o id do passageiro, ou 'T' para calcular a probabilidade de sobrevivência de todos, ou 'S' para Sair: ").upper()
+
     if id == 'S':
         print("Programa encerrado!!")
         sys.exit()
     elif id == 'T':
         for passenger in titanic_tbl:
             exibe_probabilidade(passenger, titanic_tbl)
+  
         print("Total de passageiros: ", len(titanic_tbl))
         sys.exit()
+ 
     passageiro = [passg for passg in titanic_tbl if passg["PassengerId"] == id]
+
     if len(passageiro) == 0:
         id = ''
         print("Id não encontrado!!")
